fe6706fd5562bc68b077f79c851b7916f0ceea45
Rob Tucker <rtucker@mozilla.com>
2012-04-25 08:19:51 -0700

    Fixed incorrect url for dcops_callendar_shipping typo

--- apps/dcops_calendar/urls.py
+++ apps/dcops_calendar/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns("",
     url(r"^expected_completion[/]$", 'dcops_calendar.views.expected_completion', name="dcops_calendar_expected_completion"),
     url(r"^expected_start[/]$", 'dcops_calendar.views.expected_start', name="dcops_calendar_expected_start"),
-    url(r"^shipping/]$", 'dcops_calendar.views.shipping', name="dcops_calendar_shipping"),
+    url(r"^shipping[/]$", 'dcops_calendar.views.shipping', name="dcops_calendar_shipping"),
 )
